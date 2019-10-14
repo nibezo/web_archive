a9f52065dac41653170f184900985fd7a1e9d1c4
Alexander <31167599+ultimatulny@users.noreply.github.com>
2019-10-14 23:43:47 +0300

    release

--- 0to100/script.js
+++ 0to100/script.js
@@ -1,13 +1,5 @@
 let currentNum = 1;
 let last_user = [];
-let turn1 = [];
-let turn2 = [];
-let turn3 = [];
-let turn4 = [];
-let turn5 = [];
-let turn6 = [];
-let turn7 = [];
-let turn8 = [];
 
 let arr = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -37,6 +29,8 @@ function printMatrix(){
 // Отслеживание клика по блоку
 $('.block').on('click', function(){ 
 
+
+
 	let id = $(this).attr("value");
 	let i = id[0];
 	let j = id[1];
@@ -48,10 +42,18 @@ $('.block').on('click', function(){
 	}
 
 	checkTurn(i, j);
-	
 });
 
 function checkTurn(i, j){
+	let turn1 = [];
+	let turn2 = [];
+	let turn3 = [];
+	let turn4 = [];
+	let turn5 = [];
+	let turn6 = [];
+	let turn7 = [];
+	let turn8 = [];
+
 	let last_i = +last_user[0];
 	let last_j = +last_user[1];
 
@@ -75,7 +77,88 @@ function checkTurn(i, j){
 	   ((i == turn7[0]) && (j == turn7[1])) || 
 	   ((i == turn8[0]) && (j == turn8[1]))){
 		userTurn(i, j);
+
 	}
+
+}
+
+function colorTurn(i, j){
+	let ii = +i;
+	let jj = +j;
+	let turn1 = [];
+	let turn2 = [];
+	let turn3 = [];
+	let turn4 = [];
+	let turn5 = [];
+	let turn6 = [];
+	let turn7 = [];
+	let turn8 = [];
+
+	turn1[0] = ii - 2; turn1[1] = jj - 1;
+	turn2[0] = ii - 2; turn2[1] = jj + 1;
+	turn3[0] = ii - 1; turn3[1] = jj - 2;	
+	turn4[0] = ii - 1; turn4[1] = jj + 2;
+	turn5[0] = ii + 1; turn5[1] = jj - 2;
+	turn6[0] = ii + 1; turn6[1] = jj + 2;
+	turn7[0] = ii + 2; turn7[1] = jj - 1;
+	turn8[0] = ii + 2; turn8[1] = jj + 1;
+
+	if((turn1[0] >= 0) && (turn1[1] >= 0)){
+		if(arr[turn1[0]][turn1[1]] != 0) 
+			$("div[value='"+turn1[0]+turn1[1]+"']").addClass('redBack');
+		else
+			$("div[value='"+turn1[0]+turn1[1]+"']").addClass('greenBack');
+	}
+
+	if((turn2[0] >= 0) && (turn2[1] >= 0)){
+		if(arr[turn2[0]][turn2[1]] != 0) 
+			$("div[value='"+turn2[0]+turn2[1]+"']").addClass('redBack');
+		else
+			$("div[value='"+turn2[0]+turn2[1]+"']").addClass('greenBack');
+	}
+
+	if((turn3[0] >= 0) && (turn3[1] >= 0)){
+		if(arr[turn3[0]][turn3[1]] != 0) 
+			$("div[value='"+turn3[0]+turn3[1]+"']").addClass('redBack');
+		else
+			$("div[value='"+turn3[0]+turn3[1]+"']").addClass('greenBack');
+	}
+
+	if((turn4[0] >= 0) && (turn4[1] >= 0)){
+		if(arr[turn4[0]][turn4[1]] != 0) 
+			$("div[value='"+turn4[0]+turn4[1]+"']").addClass('redBack');
+		else
+			$("div[value='"+turn4[0]+turn4[1]+"']").addClass('greenBack');
+	}
+
+	if((turn5[0] >= 0) && (turn5[1] >= 0)){
+		if(arr[turn5[0]][turn5[1]] != 0) 
+			$("div[value='"+turn5[0]+turn5[1]+"']").addClass('redBack');
+		else
+			$("div[value='"+turn5[0]+turn5[1]+"']").addClass('greenBack');
+	}
+
+	if((turn6[0] >= 0) && (turn6[1] >= 0)){
+		if(arr[turn6[0]][turn6[1]] != 0) 
+			$("div[value='"+turn6[0]+turn6[1]+"']").addClass('redBack');
+		else
+			$("div[value='"+turn6[0]+turn6[1]+"']").addClass('greenBack');
+	}
+
+	if((turn7[0] >= 0) && (turn7[1] >= 0)){
+		if(arr[turn7[0]][turn7[1]] != 0) 
+			$("div[value='"+turn7[0]+turn7[1]+"']").addClass('redBack');
+		else
+			$("div[value='"+turn7[0]+turn7[1]+"']").addClass('greenBack');
+	}
+
+	if((turn8[0] >= 0) && (turn8[1] >= 0)){
+		if(arr[turn8[0]][turn8[1]] != 0) 
+			$("div[value='"+turn8[0]+turn8[1]+"']").addClass('redBack');
+		else
+			$("div[value='"+turn8[0]+turn8[1]+"']").addClass('greenBack');
+	}
+
 }
 
 
@@ -87,7 +170,11 @@ function userTurn(i, j){
 	last_user[0] = i;
 	last_user[1] = j;
 	drawMap();
+	$('.block').removeClass('greenBack');
+	$('.block').removeClass('redBack');
+	colorTurn(i, j);
 }
+
 
 // Отображает текущее положение поля
 function drawMap(){
